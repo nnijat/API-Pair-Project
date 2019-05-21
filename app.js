0435dd78c571d48f48ef689b291a6f934dd50fb3
nnijat <nn.aca2019@gmail.com>
2019-05-21 15:38:21 -0500

    Completed Fetch API Unite Test

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 require('dotenv').config();
 const fetch = require("node-fetch");
+const assert = require('assert');
 
 const KEY = process.env.API_KEY;
 const BASE_LINK = "https://sandbox-api.brewerydb.com/v2/";
 const ENDPOINT = "beers/?key=";
-const PARAMETER = "/&beerId=";
+const PARAMETER = "&beerId=";
 
 const readline = require('readline');
 
@@ -16,26 +17,71 @@ let rl = readline.createInterface({
 });
 
 rl.question("Enter your beer ID: ", function (beerID) {
-    getBeerNameByID(beerID);
+    getBeerNameByID(fetch, beerID);
     rl.close();
 });
 
-// IDs you can user : c4f2KE/zTTWa2/zfP2fK/xwYSL2/
-function getBeerNameByID(beerID) {
-    const URL = BASE_LINK + ENDPOINT + KEY + PARAMETER + beerID;
+// IDs you can use : c4f2KE/zTTWa2/zfP2fK/xwYSL2/
+function getBeerNameByID(fetch, beerID) {
 
-    fetch(URL)
+    const URL = BASE_LINK + ENDPOINT + KEY + PARAMETER + beerID;
+    return fetch(URL)
         .then(res => res.json())
         .then(res => {
             if (res.status === "success") {
                 console.log("Beer name: ", res.data.nameDisplay);
             } else {
-                console.log("Please check your client errors!!!")
+                console.log("Cheers!!!")
             }
         })
 
         .catch(error => console.log("oops, looks like we got an error: ", error))
-        .finally(() => console.log("Did you get your beer?"))
+        .finally(() => console.log("Cheers!!!!"))
 
 }
+
+//TESTING
+describe('getBeerNameByID', () => {
+
+    // Misstyped address
+    it('1. uses the right address', () => {
+        const testURL = url => {
+            assert.deepEqual(url, BASE_LINK + ENDPOINT + KEY + PARAMETER + "c4f2KE");
+            return new Promise(function () { })
+        }
+        getBeerNameByID(testURL, 'c4f2KE')
+    });
+
+    // Out of range request (ex what the max request number is)
+    it('2. keeps all requests in range', () => {
+        const testRange = () => {
+            assert.equal(BASE_LINK + ENDPOINT + KEY + "&totalResults=" + "10000");
+            return new Promise(function () { })
+        }
+        getBeerNameByID(testRange, 'c4f2KE')
+    });
+
+    // Wrong api key (if key does not equal the exact api key)
+    it('3. uses correct api key', () => {
+        const testKEY = () => {
+            assert.equal(process.env.API_KEY, "b65e450716eea9201350284a25e5652a")
+            return new Promise(function () { })
+        }
+        getBeerNameByID(testKEY, 'c4f2KE')
+    });
+
+    //  Return wrong name
+    it('parses the response of fetch correctly', (done) => {
+        const fakeFetch = () => {
+            return Promise.resolve({
+                data: { name: "'Murican Pilsner" }
+            })
+        }
+        getBeerNameByID(fakeFetch, 'c4f2KE')
+            .then(result => {
+                assert.equal(result.data.name === "'Murican Pilsner");
+                done()
+            })
+    })
+});
 
