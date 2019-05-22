494fc92aef4c31964f50766281d811aef0e9425a
nnijat <nn.aca2019@gmail.com>
2019-05-22 18:17:23 -0500

    Finalized API Fetch Unite Test

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 require('dotenv').config();
 const fetch = require("node-fetch");
+const assert = require('assert');
 
 const KEY = process.env.API_KEY;
 const BASE_LINK = "https://sandbox-api.brewerydb.com/v2/";
 const ENDPOINT = "beers/?key=";
-const PARAMETER = "/&beerId=";
+const PARAMETER = "&beerId=";
 
 const readline = require('readline');
 
@@ -23,19 +24,83 @@ rl.question("Enter your beer ID: ", function (beerID) {
 // IDs you can user : c4f2KE/zTTWa2/zfP2fK/xwYSL2/
 function getBeerNameByID(beerID) {
     const URL = BASE_LINK + ENDPOINT + KEY + PARAMETER + beerID;
-
-    fetch(URL)
-        .then(res => res.json())
+    return fetch(URL)
+        //.then(res => res.json())
+        .then(res => {
+            return res.json()
+        })
         .then(res => {
             if (res.status === "success") {
-                console.log("Beer name: ", res.data.nameDisplay);
+                console.log("Beer name: ", res.data.name);
             } else {
                 console.log("Please check your client errors!!!")
             }
+            return res;
         })
-
         .catch(error => console.log("oops, looks like we got an error: ", error))
         .finally(() => console.log("Did you get your beer?"))
 
 }
+
+//TESTING
+describe('getBeerNameByID', () => {
+
+    // Misstyped address
+    it('1. uses the right address', () => {
+        const testURL = url => {
+            assert.deepEqual(url, BASE_LINK + ENDPOINT + KEY + PARAMETER + "c4f2KE");
+            return new Promise(function () { })
+        }
+        getBeerNameByID(testURL, 'c4f2KE')
+    });
+
+    // Out of range request (ex what the max request number is)
+    it('2. keeps all requests in range', () => {
+        const testRange = () => {
+            assert.equal(BASE_LINK + ENDPOINT + KEY + "&totalResults=" + "10000");
+            return new Promise(function () { })
+        }
+        getBeerNameByID(testRange, 'c4f2KE')
+    });
+
+    // Wrong api key (if key does not equal the exact api key)
+    it('3. uses correct api key', () => {
+        const testKEY = () => {
+            assert.equal(process.env.API_KEY, "b65e450716eea9201350284a25e5652a")
+            return new Promise(function () { })
+        }
+        getBeerNameByID(testKEY, 'c4f2KE')
+    });
+
+    // Return wrong name
+    it('4. parses the response of fetch correctly', () => {
+        function fakeFetch() {
+            return Promise.resolve({
+                json() {
+                    return {
+                        data: [
+                            { name: "12th Of Never" }
+                        ]
+                    }
+                }
+            })
+        }
+        return getBeerNameByID(fakeFetch, 'zfP2fK')
+            .then(result => assert([result.name === '12th Of Never']))
+    })
+
+    // Return wrong status
+    it('4. parses the response of fetch correctly', () => {
+        function fakeFetch() {
+            return Promise.resolve({
+                json() {
+                    return { status: "success" }
+                }
+            })
+        }
+        return getBeerNameByID(fakeFetch, 'zfP2fK')
+            .then(result => assert([result.status === 'success']))
+    })
+});
+
 
